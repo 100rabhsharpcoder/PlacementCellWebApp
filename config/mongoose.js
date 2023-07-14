571d28f757ef98fca328b8153709d81247631685
100rabhsharpcoder <saurabhgathade@gmail.com>
2023-07-14 21:11:33 +0530

    updating db url

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
  
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentPlacementCellApp');
+mongoose.connect('mongodb+srv://saurabhgathade:5M0vkOXTuAPpVIpa@cluster0.kl1fyn7.mongodb.net/');
 
 const db  =mongoose.connection;
 
 db.on('error', console.error.bind(console,"Error in connecting mongodb "));
 
 db.once('open', function(){
-    console.log('connected to database name Student Placemnet Cell')
+    console.log('connected to database  Placemnet Cell Web App')
 });
 
 
 module.exports =  db;
+
+
+// password 5M0vkOXTuAPpVIpa   saurabhgathade   url
+//   mongodb+srv://saurabhgathade:5M0vkOXTuAPpVIpa@cluster0.kl1fyn7.mongodb.net/
+// mongodb+srv://saurabhgathade:5M0vkOXTuAPpVIpa@cluster0.kl1fyn7.mongodb.net/ 
+
